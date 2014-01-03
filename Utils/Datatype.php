5d540a1ab49ba6d5733006a4f40c47faef81ee13
Guillaume <gbeauvoir@majes.fr>
2014-01-03 17:26:36 +0100

    Add file attribute

--- Utils/Datatype.php
+++ Utils/Datatype.php
@@ -5,28 +5,28 @@ use Majes\MediaBundle\Entity\Media;
 
 class Datatype
 {
-	
-	public $_content;
-	public $_ref;
-	public $_em;
-	public $_request;
-	public $_user;
-	public $_block;
+    
+    public $_content;
+    public $_ref;
+    public $_em;
+    public $_request;
+    public $_user;
+    public $_block;
 
     public function __construct($pageTemplateBlock, $block, $attributes, $em, $request, $user, $id = '', $title = ''){
 
-    	$this->_em = $em; 
-    	$this->_request = $request; 
-    	$this->_user = $user;
-    	$this->_block = $block;
+        $this->_em = $em; 
+        $this->_request = $request; 
+        $this->_user = $user;
+        $this->_block = $block;
 
-    	$this->_content = !is_null($pageTemplateBlock) ? json_decode($pageTemplateBlock->getContent(), true) : array('attributes' => array());
+        $this->_content = !is_null($pageTemplateBlock) ? json_decode($pageTemplateBlock->getContent(), true) : array('attributes' => array());
 
-    	//Id of attribute content
+        //Id of attribute content
         if(empty($id)) $id = date('YmdHis');
 
         $this->_content['attributes'][$id] = array(
-        	'title' => $title,
+            'title' => $title,
             'id' => $id);
 
         $value = array();
@@ -46,9 +46,9 @@ class Datatype
 
     public function picture($attribute, $ref){
 
-    	if(isset($attribute['remove']) && $attribute['remove']) return null;
+        if(isset($attribute['remove']) && $attribute['remove']) return null;
 
-    	$file = $this->_request->files->get('attributes');
+        $file = $this->_request->files->get('attributes');
         $file = $file[$ref]['value'];
 
         unset($attribute['value']);
@@ -65,13 +65,13 @@ class Datatype
         }
 
         if(!is_null($file)
-        	|| is_null($media)){
-        	$media = new Media();
+            || is_null($media)){
+            $media = new Media();
             $media->setCreateDate(new \DateTime(date('Y-m-d H:i:s')));
             $media->setUser($this->_user);
             $media->setFolder('Cms');
             $media->setType('picture');
-        	$media->setFile($file);
+            $media->setFile($file);
         }
 
         $title = $media->getTitle();
@@ -89,15 +89,15 @@ class Datatype
     }
 
     public function textline($attribute, $ref){
-    	return $attribute;
+        return $attribute;
     }
 
     public function textarea($attribute, $ref){
-    	return $attribute;
+        return $attribute;
     }
 
     public function editor($attribute, $ref){
-    	return $attribute;
+        return $attribute;
     }
 
     public function link($attribute, $ref){
@@ -112,6 +112,50 @@ class Datatype
             $attribute['page_id'] = $route->getPage()->getId();
         }
 
+        return $attribute;
+    }
+
+    public function file($attribute, $ref){
+
+        if(isset($attribute['remove']) && $attribute['remove']) return null;
+
+        $file = $this->_request->files->get('attributes');
+        $file = $file[$ref]['value'];
+
+        unset($attribute['value']);
+
+        $media = $this->_em->getRepository('MajesMediaBundle:Media')
+            ->findOneById($attribute['media_id']); 
+
+        if(is_null($media)){
+            $media = new Media();
+            $media->setCreateDate(new \DateTime(date('Y-m-d H:i:s')));
+            $media->setUser($this->_user);
+            $media->setFolder('Cms');
+            $media->setType('document');
+        }
+
+        if(!is_null($file)
+            || is_null($media)){
+            $media = new Media();
+            $media->setCreateDate(new \DateTime(date('Y-m-d H:i:s')));
+            $media->setUser($this->_user);
+            $media->setFolder('Cms');
+            $media->setType('document');
+            $media->setFile($file);
+        }
+
+        $title = $media->getTitle();
+        $author = $media->getAuthor();
+        if(empty($title)) $media->setTitle($attribute['title']);
+        if(empty($author)) $media->setAuthor($attribute['author']);
+        
+        $this->_em->persist($media);
+        $this->_em->flush();
+
+        $is_protected = $media->getIsProtected();
+        $attribute['path'] = $is_protected ? '' : $media->getWebPath();
+        $attribute['media_id'] = $media->getId();
         return $attribute;
     }
 }
